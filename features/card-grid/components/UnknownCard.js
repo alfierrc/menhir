e7bc52838108ea8430c1ea9cf9efaa70309551f0
alfierrc <arrcoleman95@gmail.com>
2025-08-08 09:46:44 +0100

    reboot

--- features/card-grid/components/UnknownCard.js
+++
@@ -1,6 +0,0 @@
-export function createUnknownCard(item) {
-  const card = document.createElement('div');
-  card.className = 'card';
-  card.innerText = `Unsupported type: ${item.type}`;
-  return card;
-}
