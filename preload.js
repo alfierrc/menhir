09f4d6ab8d82ae14c1faa8696ec76a4849a16249
alfierrc <arrcoleman95@gmail.com>
2025-08-08 10:01:59 +0100

    fix: define main

--- preload.js
+++ preload.js
@@ -4,13 +4,9 @@
 
   contextBridge.exposeInMainWorld('api', {
     loadVault: () => ipcRenderer.invoke('load-vault'),
+    getImagePath: (folder, filename) => ipcRenderer.invoke('get-image-path', { folder, filename }),
   });
 
   globalThis.__preloadDone = true;
   try { console.log('[preload] ready'); } catch {}
 })();
-
-contextBridge.exposeInMainWorld('api', {
-  loadVault: () => ipcRenderer.invoke('load-vault'),
-  getImagePath: (folder, filename) => ipcRenderer.invoke('get-image-path', { folder, filename }),
-});
