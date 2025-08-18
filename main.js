d23cba546790353eb794b55d7ea003993c6541bd
alfierrc <arrcoleman95@gmail.com>
2025-08-18 10:02:43 +0100

    feat(modal): edit vault folder names to onon plural

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
+const fs = require("fs");
+const matter = require("gray-matter");
 const { pathToFileURL } = require("url");
 const { scanVault } = require("./lib/vaultReader");
 
