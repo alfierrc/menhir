693bcdc0b4de44827ca004c2f12984c3e9aaa70d
alfierrc <arrcoleman95@gmail.com>
2025-08-19 08:50:53 +0100

    Turn off sandbox

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow() {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
       nodeIntegration: false,
-      sandbox: true,
+      sandbox: false,
     },
   });
   win.loadFile(path.join(__dirname, "app", "index.html"));
