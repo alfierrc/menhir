8d17be84987520a4b20800c0577e64da8f699123
alfierrc <arrcoleman95@gmail.com>
2025-08-20 16:17:43 +0100

    feat(types): fix missing import for webpage type

--- features/modal/registry.js
+++ features/modal/registry.js
@@ -1,6 +1,7 @@
 import { renderImageView } from "./views/image.js";
 import { renderNoteView } from "./views/note.js";
 import { renderProductView } from "./views/product.js";
+import { renderWebpageView } from "./views/webpage.js";
 
 export const MODAL_VIEWS = {
   image: renderImageView,
